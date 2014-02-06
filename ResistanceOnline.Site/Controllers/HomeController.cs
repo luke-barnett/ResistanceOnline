0b95e68453cae586382b6e3226b065bb1605ff2d
verneroberts <verneroberts@gmail.com>
2014-02-06 16:31:09 +1300

    thinking about site and how http requests will hook into the game

--- ResistanceOnline.Site/Controllers/HomeController.cs
+++ ResistanceOnline.Site/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ResistanceOnline.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,81 @@ namespace ResistanceOnline.Site.Controllers
 {
 	public class HomeController : Controller
 	{
-		//
-		// GET: /Home/
+        Game GetGame(int gameId)
+        {
+            //todo - something with the database :)
+            var game = new Game(6);
+
+            game.AddCharacter(Character.Assassin);
+            game.AddCharacter(Character.LoyalServantOfArthur);
+            game.AddCharacter(Character.LoyalServantOfArthur);
+            game.AddCharacter(Character.Percival);
+            game.AddCharacter(Character.Morcana);
+            game.AddCharacter(Character.Merlin);
+
+            game.JoinGame("Jordan");
+            game.JoinGame("Luke");
+            game.JoinGame("Jeffrey");
+            game.JoinGame("Simon");
+            game.JoinGame("Verne");
+            game.JoinGame("Jayvin");
+
+            return game;
+        }
+
 		public ActionResult Index()
 		{
 			return View();
 		}
+
+        public ActionResult CreateGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        //playerguid can be null for spectators
+        public ActionResult Game(int gameId, Guid? playerGuid)
+        {
+            var game = GetGame(gameId);
+
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public ActionResult AddCharacter(int gameId, Guid playerGuid, string character) 
+        {
+            throw new NotImplementedException();
+        }        
+        [HttpPost]
+        public ActionResult ProposePersonForQuest(int gameId, Guid playerGuid, int playerIdOfProposed) 
+        {
+            throw new NotImplementedException();
+        }        
+        [HttpPost]
+        public ActionResult SubmitQuestCard(int gameId, Guid playerGuid, bool success) 
+        {
+            throw new NotImplementedException();
+        }        
+        [HttpPost]
+        public ActionResult VoteForQuest(int gameId, Guid playerGuid, bool accept) 
+        {
+            throw new NotImplementedException();
+        }                         
+        [HttpPost]
+        public ActionResult AddCharacter(int gameId, Guid playerGuid, string character) 
+        {
+            throw new NotImplementedException();
+        }        
+        [HttpPost]
+        public ActionResult JoinGame(int gameId, string name) 
+        {
+            throw new NotImplementedException();
+        }        
+        [HttpPost]
+        public ActionResult GuessMerlin(int gameId, Guid playerGuid, int playerIdOfMerlin) 
+        {
+            throw new NotImplementedException();
+        }        
+
 	}
 }

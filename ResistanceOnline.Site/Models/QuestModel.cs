4018c5d090bc0c3151703c8727d473b0bf5ebbe0
Luke Barnett <luke@barnett.net.nz>
2014-02-08 19:08:59 +1300

    Updated libraries and converted html to more boostrapesk markup to make it look less like arse.
    Also added Humanizer to convert enums to nice strings :D

--- ResistanceOnline.Site/Models/QuestModel.cs
+++ ResistanceOnline.Site/Models/QuestModel.cs
@@ -5,20 +5,20 @@ using System.Text;
 
 namespace ResistanceOnline.Site.Models
 {
-    public class QuestModel
-    {
-        public string Leader { get; set; }
-        public List<string> PlayersOnQuest { get; set; }
-        public List<VoteModel> Vote { get; set; }
-        public List<QuestCardModel> QuestCards { get; set; }
+	public class QuestModel
+	{
+		public string Leader { get; set; }
+		public List<string> PlayersOnQuest { get; set; }
+		public List<VoteModel> Vote { get; set; }
+		public List<QuestCardModel> QuestCards { get; set; }
 
-        public QuestModel(Core.Quest quest, int totalPlayers)
-        {
-            Leader = quest.Leader.Name;
+		public QuestModel(Core.Quest quest, int totalPlayers)
+		{
+			Leader = quest.Leader.Name;
 
-            PlayersOnQuest = quest.ProposedPlayers.Select(p => p.Name).ToList();
-            Vote = quest.Votes.Select(v => new VoteModel(v, quest.Votes.Count != totalPlayers)).ToList();
-            QuestCards = quest.QuestCards.Select(q => new QuestCardModel(q.Success, quest.QuestCards.Count != PlayersOnQuest.Count)).ToList();
-        }
-    }
+			PlayersOnQuest = quest.ProposedPlayers.Select(p => p.Name).ToList();
+			Vote = quest.Votes.OrderBy(q => Guid.NewGuid()).Select(v => new VoteModel(v, quest.Votes.Count != totalPlayers)).ToList();
+			QuestCards = quest.QuestCards.OrderBy(q => Guid.NewGuid()).Select(q => new QuestCardModel(q.Success, quest.QuestCards.Count != PlayersOnQuest.Count)).ToList();
+		}
+	}
 }

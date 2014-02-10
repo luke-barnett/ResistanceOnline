bb47c15275325b5b5ea42261cdc84d9b55fef71d
betty <smckoy@gmail.com>
2014-02-11 07:34:47 +1300

    knockouts

--- ResistanceOnline.Site/App/main.js
+++ ResistanceOnline.Site/App/main.js
@@ -5,6 +5,8 @@
         'plugins': '/scripts/durandal/plugins',
         'transitions': '/scripts/durandal/transitions',
         'knockout': '/scripts/knockout-3.0.0',
+        'knockout.mapping': '/scripts/knockout.mapping-latest',
+        'knockout.punches': '/scripts/knockout.punches.min',
         'jquery': '/scripts/jquery-2.1.0',
         'signalr.core': '/scripts/jquery.signalR-2.0.2.min',
         'signalr.hubs': '/signalr/hubs?'

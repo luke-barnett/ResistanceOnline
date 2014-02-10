c54987334ede56758582b34311037476c8912740
betty <smckoy@gmail.com>
2014-02-10 22:55:02 +1300

    add signalr/durandal

---
+++ ResistanceOnline.Site/App/shell.js
@@ -0,0 +1,22 @@
+﻿define(['require', 'plugins/router', 'signalr.hubs'], function (require, router, signalr) {
+    
+    var update = function (games) {
+        console.log(games);
+    };
+
+    var gameHub = $.connection.gameHub;
+    gameHub.on('update', update);
+    $.connection.hub.start();
+        
+    return {
+        router: router,
+        activate: function () {
+            router.map([
+              { route: '', title: 'Home', moduleId: 'home/index', nav: true },
+              { route: 'game/:gameId', title: 'Game', moduleId: 'game/index', nav: true },
+            ]).buildNavigationModel();
+
+            return router.activate();
+        }
+    };
+});
--- ResistanceOnline.Site/App_Start/BundleConfig.cs
+++ ResistanceOnline.Site/App_Start/BundleConfig.cs
@@ -10,7 +10,7 @@ namespace ResistanceOnline.Site
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-
+          
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 

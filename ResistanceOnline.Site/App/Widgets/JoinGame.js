76554adf7ba6597481b238d073a64e65473c790b
betty <smckoy@gmail.com>
2014-02-12 07:31:24 +1300

    join game

---
+++ ResistanceOnline.Site/App/Widgets/JoinGame.js
@@ -0,0 +1,17 @@
+﻿define(['data', 'knockout', 'knockout.punches'], function (data, ko, kop) {
+    console.log('not u');
+    //todo ctor?
+    var viewModel = {
+        gameId: ko.observable(),
+        name: ko.observable(''),
+        joinGame: function () {
+            $.connection.gameHub.server.joinGame(viewModel.gameId(), viewModel.name());
+            //player guid?
+        },
+        activate: function(game) {            
+            viewModel.gameId(game.GameId());
+        }
+    };
+
+    return viewModel;
+});

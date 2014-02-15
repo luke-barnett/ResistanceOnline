f55976d1ab111255a6fa16c96365fe24c24f4d57
verne <verneroberts@gmail.com>
2014-02-16 12:30:49 +1300

    bots are more entertaining

--- ResistanceOnline.Site/ListExtensions.cs
+++ ResistanceOnline.Site/ListExtensions.cs
@@ -7,6 +7,7 @@ namespace ResistanceOnline.Site
 {
     public static class ListExtensions
     {
+        private static Random _random = new Random();
         public static T Random<T>(this IEnumerable<T> list)
         {
             return list.Random(x => true);
@@ -14,12 +15,12 @@ namespace ResistanceOnline.Site
 
         public static T Random<T>(this IEnumerable<T> list, Func<T, bool> predicate)
         {
-            return (T)list.OrderBy(x => new Random().Next()).First(predicate);
+            return (T)list.OrderBy(x => _random.Next()).First(predicate);
         }
 
         public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> list)
         {
-            return list.OrderBy(x => new Random().Next());
+            return list.OrderBy(x => _random.Next());
         }
 
 

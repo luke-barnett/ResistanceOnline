58faca2955b520fa8ea0c4e97ff475aacb3fc0d8
verneroberts <verneroberts@gmail.com>
2014-03-10 15:44:16 +1300

    added some database shiz

--- ResistanceOnline.Site/Controllers/GameController.cs
+++ ResistanceOnline.Site/Controllers/GameController.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Game = ResistanceOnline.Core.Game;
 
 namespace ResistanceOnline.Site.Controllers
 {

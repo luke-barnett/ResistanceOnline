f6c72a43e6c61517f0df8df7a05145dc0f46f279
Luke Barnett <luke@barnett.net.nz>
2014-04-05 17:53:02 +1300

    #19 Added support for passing through smtp details in app settings (which can be defined in Azure)
    If partial or none are present it will fallback to default stmp settings

--- ResistanceOnline.Site/Infrastructure/Messaging/Implementations/EmailService.cs
+++ ResistanceOnline.Site/Infrastructure/Messaging/Implementations/EmailService.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNet.Identity;
 using Postal;
 using ResistanceOnline.Database.Entities;
+using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -29,7 +32,53 @@ namespace ResistanceOnline.Site.Infrastructure.Messaging.Implementations
 			email.To = emailClaim.Value;
 			email.Subject = subject;
 			email.Message = message;
-			email.Send();
+
+			var service = new Postal.EmailService(System.Web.Mvc.ViewEngines.Engines, () =>
+			{
+				if (string.IsNullOrWhiteSpace(SMTPServer) || string.IsNullOrWhiteSpace(SMTPUsername) || string.IsNullOrWhiteSpace(SMTPPassword) || string.IsNullOrWhiteSpace(SMTPPort))
+				{
+					return new SmtpClient();
+				}
+
+				return new SmtpClient(SMTPServer, int.Parse(SMTPPort))
+				{
+					Credentials = new NetworkCredential(SMTPUsername, SMTPPassword)
+				};
+			});
+
+			await service.SendAsync(email);
+		}
+
+		string SMTPServer
+		{
+			get
+			{
+				return ConfigurationManager.AppSettings["SMTPServer"];
+			}
+		}
+
+		string SMTPPort
+		{
+			get
+			{
+				return ConfigurationManager.AppSettings["SMTPPort"];
+			}
+		}
+
+		string SMTPUsername
+		{
+			get
+			{
+				return ConfigurationManager.AppSettings["SMTPUsername"];
+			}
+		}
+
+		string SMTPPassword
+		{
+			get
+			{
+				return ConfigurationManager.AppSettings["SMTPPassword"];
+			}
 		}
 	}
 }
